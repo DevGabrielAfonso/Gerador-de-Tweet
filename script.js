92815ae548d99c4916329cb4bd5f1a706cc7e4c5
DevGabrielAfonso <gabriel.sussia0205@gmail.com>
2026-02-02 03:44:51 -0300

    Mudando a ideia para 'Gerador de Tweet'

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const sujeitos = ["Usuários", "Vagabundos", "Amigos", "Jogadores", "Pessoas", "Políticos"]
+const verbos = ["fazem", "compram", "gostam de", "bebem", "odeiam", "usam"]
+const substantivos = ["coisas", "água", "comida", "cachorros", "crime", "banana", "bolo", "computador"]
+const adjetivos = ["perfeitos", "loucos", "terríveis", "maníacos", "gostosos", "uma ameaça"]
+
 function randomizar(){
-    let text = document.getElementsByClassName("tweetarea")[0].value;
-    let finalText = text.trim().split(/\s+/);  
-    for (let i = finalText.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [finalText[i], finalText[j]] = [finalText[j], finalText[i]];
-    }
-    document.getElementsByClassName("tweetarea")[0].value = finalText.join(' ');
+    let frase = "";
+    frase += sujeitos[Math.floor(Math.random()*sujeitos.length)];
+    frase += " que "
+    frase += verbos[Math.floor(Math.random()*verbos.length)];
+    frase += " "
+    frase += substantivos[Math.floor(Math.random()*substantivos.length)];
+    frase += " são "
+    frase += adjetivos[Math.floor(Math.random()*adjetivos.length)];
+    document.getElementsByClassName("tweetarea")[0].value = frase;
 }
