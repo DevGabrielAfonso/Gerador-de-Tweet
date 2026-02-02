771522adfddb9bf58cc6179b3f758a129b9328c9
DevGabrielAfonso <gabriel.sussia0205@gmail.com>
2026-02-02 20:02:25 -0300

    FIX: botão de tweet funcionando em mobile

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ const label = document.getElementsByClassName("label");
 
 function randomizar(){
     //frase
-    let frase = "";
+    var frase = "";
     frase += sujeitos[Math.floor(Math.random()*sujeitos.length)] + " que ";
     frase += verbos[Math.floor(Math.random()*verbos.length)] + " ";
     frase += substantivos[Math.floor(Math.random()*substantivos.length)] + " são ";
     frase += adjetivos[Math.floor(Math.random()*adjetivos.length)];
     document.getElementsByClassName("tweetarea")[0].value = frase;
-    document.getElementById("tweetar").href = "https://x.com/intent/post?text="+ frase
-
+    
     //hora
     let time = new Date();
     let hora = adicionarZero(time.getHours());
@@ -21,8 +20,16 @@ function randomizar(){
     let dia = adicionarZero(time.getDate());
     let mes = adicionarZero(time.getMonth()+1);
     let ano = time.getFullYear();
-
+    
     document.getElementById("time").innerHTML = `${hora}:${minutos} ${dia}/${mes}/${ano}`
+}
+
+function tweetar(){
+    const frase = document.getElementsByClassName("tweetarea")[0].value;
+    const fraseParaUrl = encodeURIComponent(frase)
+    if (fraseParaUrl.length == 0){return 0}
+    const url = "https://x.com/intent/post?text="+ encodeURIComponent(frase);
+    window.open(url, "_blank");
 }
 
 function adicionarZero(n){
